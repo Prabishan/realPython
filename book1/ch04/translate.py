e1da11d6588b70fd3447ae31cb5f39f9309481f2
prabishan <prabishan>
2018-12-15 15:46:36 -0600

    'done'

---
+++ book1/ch04/translate.py
@@ -0,0 +1,10 @@
+user_input = input("Enter some text: ");
+user_input=user_input.replace("a",str(4));
+user_input=user_input.replace("b",str(8));
+user_input=user_input.replace("e",str(3));
+user_input=user_input.replace("l",str(1));
+user_input=user_input.replace("o",str(0));
+user_input=user_input.replace("s",str(5));
+user_input=user_input.replace("t",str(7));
+print(user_input);
+
