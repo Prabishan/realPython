fd5c1e03e6a73ae09e668ac65ce1e3d49947ea79
Prabishan <prabishan@github.com>
2018-12-16 16:27:20 -0600

    docstring added to the function

--- book1/ch05/temperature.py
+++ book1/ch05/temperature.py
@@ -1,7 +1,9 @@
 def celsius_to_fahrenheit(C):
+    """ Converts celsius to farhenheit degree"""
     F = C*9/5+32
     return F
 def fahrenheit_to_celsius(F):
+    """ Converts farhenheit degree celsius degree"""
     C=(F-32)*(5/9)
     return C
 temp1 = 72;
