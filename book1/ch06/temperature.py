b647522d25a699759c06c3b4af95c0bc4353fbd3
Prabishan <Prabishan123@gmail.com>
2018-12-16 15:42:31 -0600

    temperature file done

--- book1/ch06/temperature.py
+++ book1/ch06/temperature.py
@@ -1,6 +1,10 @@
 def celsius_to_fahrenheit(C):
-    F = C*(9/5)+32
+    F = C*9/5+32
     return F
 def fahrenheit_to_celsius(F):
     C=(F-32)*(5/9)
     return C
+temp1 = 72;
+print("{} degrees F = {} degrees C".format(temp1,fahrenheit_to_celsius(temp1)));
+temp2 = 37;
+print("{} degrees C = {} degrees F".format(temp2,celsius_to_fahrenheit(temp2)));
