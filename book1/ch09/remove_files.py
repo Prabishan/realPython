f016d3ae066ae8e3d8784201acb535d83f9b260b
Prabishan <Prabishan123@gmail.com>
2019-01-05 14:11:38 -0600

    Add files via upload

--- book1/ch09/remove_files.py
+++ book1/ch09/remove_files.py
@@ -1,2 +1,15 @@
 #Using pattern matching to delete files
 import os
+import glob
+
+my_path=r"C:\Users\Prabishan\Desktop\realPython\realPython\book1\ch09\little pics"
+
+for current_folder, subfolder, file_names in os.walk(my_path):
+    for file in file_names:
+        all_files = os.path.join(current_folder,file)
+        if all_files[len(all_files)-4:len(all_files)]==".jpg":
+            file_size = os.path.getsize(all_files)
+            if file_size < 2000:
+                print("{} deleting",all_files)
+                os.remove(all_files)
+                
